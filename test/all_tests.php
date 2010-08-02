16835883f94345798658907e0cd0873f23d8e530
Gaetan Voyer-Perrault <gatesvp@gmail.com>
2010-08-01 23:27:55 -0500

    Basic CRUD and deletions are working

--- test/all_tests.php
+++ test/all_tests.php
@@ -5,7 +5,7 @@ require_once('simpletest/autorun.php');
 class AllTests extends TestSuite{
   function AllTests(){
     $this->TestSuite('All Tests');
-    #$this->addFile('test_mongo_base.php');
+    $this->addFile('test_mongo_entity_basic.php');
   }
 }
 
